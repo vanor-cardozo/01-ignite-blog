d15406d498f44ad45be791251bc1624a20f58f24
Vanor Cardozo <vanor.fc@gmail.com>
2022-09-29 12:05:09 -0300

    feat: Componente Sidebar e sua estilização CSS

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar
+          hasBorder
+          src="https://github.com/vanor-cardozo.png"
+        />
+        <strong>Vanor Cardozo</strong>
+        <span>Desenvolvedor Front-end</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
